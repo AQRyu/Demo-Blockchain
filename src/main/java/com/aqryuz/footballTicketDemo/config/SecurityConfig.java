74ed8fa4191507f1cb818d10bb405507e25ac6f9
AQRyu <daithanh.hua@gmail.com>
2018-12-13 19:42:38 +0700

    config cors

--- src/main/java/com/aqryuz/footballTicketDemo/config/SecurityConfig.java
+++ src/main/java/com/aqryuz/footballTicketDemo/config/SecurityConfig.java
@@ -12,7 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/css/**", "/index","/js/**","/img/**").permitAll()		
+		.antMatchers("/css/**", "/index","/js/**","/img/**").permitAll()	
+		.antMatchers("/api/**").permitAll()
 		.antMatchers("/admin/**").hasRole("ADMIN")
 		.and()
 		.formLogin();
